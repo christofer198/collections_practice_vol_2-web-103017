e0326428f3e28c7a3c885256e6805fc8af3b2049
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-20 01:57:56 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,8 +49,10 @@ end
 
 def merge_data(arr1, arr2)
   name_array = []
+  count = 0
   arr1.each do |key|
-    return arr1[0].merge(arr2[0][key[:first_name]])
+    return arr1[count].merge(arr2[count][key[:first_name]])
+    count += 1
   end
   #return arr1[0].merge(arr2[0]["blake"])
 
